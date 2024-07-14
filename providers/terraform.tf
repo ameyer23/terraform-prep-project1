fdb0f2ca07eccff087cd72ab09e1a4920a984895
Andrea Meyer Munoz <andreameyer23@gmail.com>
2024-07-14 15:29:00 -0500

    Added local and remote exec provisioners.

--- providers/terraform.tf
+++ providers/terraform.tf
@@ -8,8 +8,8 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      #version = "~> 3.0"           #initial constratint
-      version = "3.76.1"            #latest version
+      version = ">= 3.0"           #initial constratint
+      #version = "3.76.1"            #latest version
     }
   }
 }
---
+++ terraform.tf
@@ -0,0 +1,32 @@
+
+# Setting Terraform and AWS Provider vesions 
+# Helpful for when codebase is not yet updated to keep up with new versions
+# This downlods the latest AWS version that matches the constraint
+
+terraform {
+  required_version = ">= 1.0.0"
+  required_providers {
+    aws = {                          #AWS provider
+      source  = "hashicorp/aws"
+      version = ">= 3.0"             #initial constratint
+      #version = "3.76.1"            #latest version
+    }
+    http = {                          #HTTP provider
+      source  = "hashicorp/http"    
+      version = "3.4.3"
+    }
+    random = {
+      source = "hashicorp/random"
+      version = "3.6.2"     #former 3.6.2
+    }
+    local = {                         #used to manage local resources
+      source = "hashicorp/local"
+      version = "2.5.1"
+    }
+    tls = {
+      source = "hashicorp/tls"       #used to create SSH Key
+      version = "4.0.5"
+    }
+  }
+}
+
